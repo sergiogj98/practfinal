63beaa92c113c01f548b866effc37cfd48e7bcb4
Sergio Guillén Jiménez <sergiogj98@uma.es>
2022-12-08 21:35:29 +0100

    Se ha añadido modificación para hacer busqueda de letras en bloque yt se ha añadido función para su testing

--- myproject/search4web.py
+++ myproject/search4web.py
@@ -8,6 +8,22 @@ def search4letters(phrase: str, letters: str = 'aeiou') -> set:
     """ Return any letters found in phrase """
     return set(letters).intersection(set(phrase))
 
+def search4letters_upgrade(phrase: str, letters: str = 'aeiou') -> set:
+    """ Return any letters found in phrase """
+    phrases=phrase.split(".")
+    rst=[]
+    phra=[]
+    results=[]
+    for phr in phrases:
+        if phr != "":
+            phra.append(phr)
+            rst.append(set(letters).intersection(set(phr)))
+    results.append(phra)
+    results.append(rst)
+
+    print(results)
+    return results
+
 def log_request(req: 'flask_request', res: str) -> None:
     """ logger for web operations """
     with open('search.log','a') as log:
--- myproject/test_search.py
+++ myproject/test_search.py
@@ -2,10 +2,14 @@ import search4web
 import unittest
 
 class TestSearch(unittest.TestCase):
-    def test_add(self):
-        # tests for the add() function
+    def test_search(self):
         self.assertEqual(search4web.search4letters("hola","aeiou"), {'o','a'})
         self.assertEqual(search4web.search4letters("holiii", "aeiou"), {'o','i'})
         self.assertEqual(search4web.search4letters("holita", "aeiou"), {'o', 'i', 'a'})
         self.assertEqual(search4web.search4letters("murcielago", "aeiou"), {'u', 'i', 'e', 'a', 'o'})
 
+    def test_search_up(self):
+        self.assertEqual(search4web.search4letters_upgrade("hola me llamo sergio. Y tu?","aeiou"), [['hola me llamo sergio', ' Y tu?'], [{'a', 'e', 'i', 'o'}, {'u'}]])
+        self.assertEqual(search4web.search4letters_upgrade("Adios Maribel. Ten cuidado", "aeiou"), [['Adios Maribel', ' Ten cuidado'], [{'e', 'i', 'a', 'o'}, {'e', 'i', 'u', 'o', 'a'}]])
+        self.assertEqual(search4web.search4letters_upgrade("No me lo puedo creer. Has sido capaz","aeiou"), [['No me lo puedo creer', ' Has sido capaz'], [{'u', 'o', 'e'}, {'o', 'i', 'a'}]])
+        self.assertEqual(search4web.search4letters_upgrade("Chicos. A cenar.","aeiou"), [['Chicos', ' A cenar'], [{'i', 'o'}, {'a', 'e'}]])
